3fdbefd14d5f3f24fdaff484cf17093e29e57c86
shashwattech <55654957+shashwattech@users.noreply.github.com>
2026-05-15 15:11:59 +0530

    Add: python-arithmetic-operators - submission-1

---
+++ Python For Beginners/python-arithmetic-operators/submission-1.py
@@ -0,0 +1,12 @@
+a, b, c = 2, 2, 0.5
+
+firstask = a + b + c
+secondask = 0 - (firstask)
+thirdask = a * b * c
+fourthask = (firstask/thirdask)
+
+
+print(firstask)
+print(secondask)
+print(thirdask)
+print(fourthask)
